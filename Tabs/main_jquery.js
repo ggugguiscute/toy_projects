41a13d7860b1800e1ecec9bddd01cc9514620bbe
ggugguiscute <kimgukwoo@gmail.com>
2022-11-16 12:51:11 +0900

    1116 Tabs

---
+++ Tabs/main_jquery.js
@@ -0,0 +1,8 @@
+$(function () {
+  //   const btn = $(".btn");
+  //   console.log(btn);
+  $(".btn").click(function () {
+    $(".btn").removeClass("active");
+    $(this).addClass("active");
+  });
+});
